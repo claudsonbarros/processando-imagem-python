491010046cfe9d3e6d7b9dd84cd6c96c98ef9f57
Claudson Barros <43166409+claudsonbarros@users.noreply.github.com>
2024-09-25 14:10:28 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,15 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as f:
-    page_description = f.read()
-
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
-
 setup(
-    name="package_name",
+    name="imagem_processor",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="claudsonbarros",
+    author_email="claudson.s.b@gmail.com",
+    description="Pacote para processamento de imagem em Python.",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/claudsonbarros/processando-imagem-python"
     packages=find_packages(),
-    install_requires=requirements,
-    python_requires='>=3.8',
+    install_requires=requirements
 )
+
